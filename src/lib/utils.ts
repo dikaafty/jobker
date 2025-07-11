8ab8df502431c977fcbf10c1269c6109454cf577
Dika Afty <dikaafty@gmail.com>
2025-07-11 23:00:53 +0700

    Export cn function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-const cn = (...inputs: ClassValue[]): string => {
+export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs));
 }
 
