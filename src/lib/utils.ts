ea48b5ea32ec06b24ed4a5705eb837a45aaa6741
Dika Afty <dikaafty@gmail.com>
2025-07-11 20:58:23 +0700

    Create runObserver function and its logic

---
+++ src/lib/utils.ts
@@ -0,0 +1,28 @@
+const runObserver = (...refs: React.RefObject<HTMLElement>[]): () => void => {
+  const options = {
+    root: null,
+    threshold: 0.5
+  }
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const animationClass: string | undefined = (entry.target as HTMLElement).dataset.animate;
+
+      if(animationClass !== undefined) {
+        entry.target.classList.toggle(animationClass, entry.isIntersecting);
+      }
+    });
+  }, options);
+
+  refs.forEach((ref) => {
+    if (Array.isArray(ref.current)) {
+      ref.current.forEach((r: HTMLElement) => {
+        if (r) observer.observe(r);
+      });
+    } else {
+      if (ref.current) observer.observe(ref.current);
+    }
+  });
+
+  return () => observer.disconnect();
+}
