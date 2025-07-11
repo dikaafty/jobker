54cfbd4dc52e5f2dadb0d02761e5db4fe47d0d00
Dika Afty <dikaafty@gmail.com>
2025-07-11 21:25:18 +0700

    Create brand logo jsx and its classNames in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,11 @@ const Navbar = () => {
     <nav 
       className="w-full flex justify-between items-center px-4 py-8"
     >
-      
+      <h1 
+        className="text-lg font-jost font-extrabold"
+      >
+        JOBKER.
+      </h1>
     </nav>
   )
 }
