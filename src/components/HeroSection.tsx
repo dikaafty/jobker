987172b56e15427d94b557f9f5afe2486d8b7fd1
Dika Afty <dikaafty@gmail.com>
2025-07-12 21:12:36 +0700

    Add py-25 className to section jsx in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <section className="flex flex-col justify-center items-center px-8 sm:px-10">
+    <section className="flex flex-col justify-center items-center px-8 sm:px-10 py-25">
       <h1
         className="font-jost font-black text-xl sm:text-3xl md:text-4xl text-center"
       >
