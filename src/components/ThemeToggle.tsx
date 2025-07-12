1541e8b76d05090999080cc96eca04fbb912dbc4
Dika Afty <dikaafty@gmail.com>
2025-07-12 15:57:27 +0700

    Create isDarkMode state and its setter in ThemeToggle component

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,6 +2,8 @@ import { Moon, Sun } from "lucide-react";
 import { useState, useEffect } from "react";
 
 const ThemeToggle = () => {
+  const [ isDarkMode, setIsDarkMode ] = useState<boolean>(true);
+
   return (
     <div>
       
