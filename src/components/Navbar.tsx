6a9838c90acb7c39f6dcf8873b211d69775d7f3d
Dika Afty <dikaafty@gmail.com>
2025-07-12 13:59:29 +0700

    Create mobile nav jsx in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,6 +30,55 @@ const Navbar = () => {
           About
         </a>
       </div>
+
+      {/* Mobile Nav */}
+      <button 
+        className="w-8 h-7 flex flex-col justify-evenly items-center z-50 md:hidden"
+        onClick={() => setIsMenuOpen(prev => !prev)}
+      >
+        <div 
+          className={cn(
+            "w-[80%] h-0.5 bg-foreground transition-all duration-300 rounded-full",
+            isMenuOpen ? "-rotate-45 translate-y-[5px]" : "",
+          )}
+        />
+        <div 
+          className={cn(
+            "w-[80%] h-0.5 bg-foreground transition-all duration-300 rounded-full",
+            isMenuOpen ? "rotate-45 -translate-y-[5px]" : "",
+          )}
+        />
+      </button>
+
+      <div 
+        className={cn(
+          "w-full h-full bg-background text-lg font-bold flex flex-col justify-center",
+          "items-center gap-5 fixed -right-190 top-0 z-10 transition-all duration-500 md:hidden",
+          isMenuOpen ? "right-0 pointer-events-auto" : "-right- 190pointer-events-none"
+        )}
+      >
+        <div className="w-1/2 flex flex-col justify-center items-center gap-2 mb-5">
+          <div className="w-40 h-0.5 bg-foreground rounded-full" />
+          <div className="w-32 h-0.5 bg-foreground rounded-full" />
+        </div>
+
+        <a href="/job">
+          JOB
+        </a>
+        
+        <a href="/job">
+          ADD JOB
+        </a>
+
+        <a href="/about">
+          ABOUT
+        </a>
+
+        <div className="w-1/2 flex flex-col justify-center items-center gap-2 mt-5">
+          <div className="w-32 h-0.5 bg-foreground rounded-full" />
+          <div className="w-40 h-0.5 bg-foreground rounded-full" />
+        </div>
+      </div>
     </nav>
   )
 }
