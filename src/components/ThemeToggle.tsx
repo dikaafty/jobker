808ac3b7443ca5d7d21d9719d967a1fa623d1fb5
Dika Afty <dikaafty@gmail.com>
2025-07-12 16:18:26 +0700

    Create toggleTheme function and its logic in ThemeToggle component

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,6 +4,18 @@ import { useState, useEffect } from "react";
 const ThemeToggle = () => {
   const [ isDarkMode, setIsDarkMode ] = useState<boolean>(true);
 
+  const toggleTheme = (): void => {
+    if(isDarkMode) {
+      document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  }
+
   return (
     <div>
       
