0c81577cb0a06845386da3213db4b9283031d09c
Dika Afty <dikaafty@gmail.com>
2025-07-12 20:59:34 +0700

    Create hero title and its items jsx in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,11 @@
 const HeroSection = () => {
   return (
     <section className="flex flex-col justify-center items-center">
-      
+      <h1
+        className="font-jost font-black text-xl sm:text-2xl md:text-3xl text-center"
+      >
+        <span className="text-fire">YOUR CAREER</span> DASHBOARD STARTS HERE
+      </h1>
     </section>
   )
 }
