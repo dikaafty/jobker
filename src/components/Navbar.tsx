ce219bdd8fa5895bb017b682fd78904a95f9012b
Dika Afty <dikaafty@gmail.com>
2025-07-12 16:22:36 +0700

    Import ThemeToggle component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { cn } from "@/lib/utils";
+import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   const [ isMenuOpen, setIsMenuOpen ] = useState<boolean>(false);
