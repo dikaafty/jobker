0eb2f197b31ff4efd1392d6e8a98e1c969ea73c6
Dika Afty <dikaafty@gmail.com>
2025-07-12 22:11:51 +0700

    Add and delete classNames in section jsx in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <section className="flex flex-col justify-center items-center px-8 sm:px-10 py-25">
+    <section className="h-[calc(100vh-72px)] flex flex-col justify-center items-center px-8 sm:px-10 pb-10">
       <h1
         className="font-jost font-black text-xl sm:text-3xl md:text-4xl text-center"
       >
