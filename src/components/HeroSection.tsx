6125a5a161432e5213951c5f0db7264e70eba674
Dika Afty <dikaafty@gmail.com>
2025-07-13 20:07:43 +0700

    Import NavLink component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const HeroSection = () => {
   return (
     <section className="h-[calc(100vh-72px)] flex flex-col justify-center items-center px-8 sm:px-10 pb-10">
