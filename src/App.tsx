9009b2aa33315ba0073b505af22d5f581cd0c509
Dika Afty <dikaafty@gmail.com>
2025-07-13 19:45:58 +0700

    Import Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NotFound from "@/pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeroSection from "@/components/HeroSection";
 import JobTrackerSection from "@/components/JobTrackerSection";
+import Navbar from "@/components/Navbar";
 
 const App = () => {
   return (
