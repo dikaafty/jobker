33ea2bb0fc966fd91875c14a3b8958dbd95dcacd
Dika Afty <dikaafty@gmail.com>
2025-07-13 19:50:28 +0700

    Import AboutSection component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeroSection from "@/components/HeroSection";
 import JobTrackerSection from "@/components/JobTrackerSection";
 import Navbar from "@/components/Navbar";
+import AboutSection from "@/components/AboutSection";
 
 const App = () => {
   return (
