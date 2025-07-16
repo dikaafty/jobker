e3e28d4da2b76110abe9b8bb11653b2fd0510bba
Dika Afty <dikaafty@gmail.com>
2025-07-16 22:38:48 +0700

    Add classNames to section jsx in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <section>
+    <section className="h-[calc(100vh-72px)] flex flex-col justify-center items-center px-8 sm:px-10 pb-10">
       
     </section>
   )
