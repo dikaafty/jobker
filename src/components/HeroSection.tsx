8e299b57e58695ed5cd64e443efabdf541069837
Dika Afty <dikaafty@gmail.com>
2025-07-16 22:39:59 +0700

    Create hero title jsx in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,11 @@
 const HeroSection = () => {
   return (
     <section className="h-[calc(100vh-72px)] flex flex-col justify-center items-center px-8 sm:px-10 pb-10">
-      
+      <h1
+        className="font-jost font-black text-xl sm:text-3xl md:text-4xl text-center"
+      >
+        <span className="text-fire">YOUR CAREER</span> DASHBOARD STARTS HERE
+      </h1>
     </section>
   )
 }
