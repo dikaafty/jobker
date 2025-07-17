8d343de0ecd1fcd9abfe532de1f238acb0fec331
Dika Afty <dikaafty@gmail.com>
2025-07-17 21:49:39 +0700

    Create desktop filter categories and its items jsx in FilterCategoriesButton component

--- src/components/FilterCategoriesButton.tsx
+++ src/components/FilterCategoriesButton.tsx
@@ -8,7 +8,44 @@ interface FilterCategoriesButtonProps {
 const FilterCategoriesButton = ({ filterCategories }: FilterCategoriesButtonProps) => {
   return (
     <div>
-      
+      {/* Desktop filter categories */}
+
+      <div className="w-full hidden md:flex justify-center items-center gap-4 lg:gap-8 px-10 pt-5">
+        {
+          filterCategories.map((filterCategory) => (
+            <div 
+              className={cn(
+                "filter-category-button border-1 transition-colors duration-300",
+                filterCategory.numberOfItems !== 0 
+                ? "border-primary cursor-pointer hover:bg-secondary" 
+                : "border-muted"
+              )}
+            >
+              <p 
+                className={cn(
+                  "text-[10px] lg:text-xs font-jost font-medium tracking-wider",
+                  filterCategory.numberOfItems === 0 
+                  ? "text-muted" 
+                  : "text-primary"
+                )}
+              >
+                {filterCategory.category}
+              </p>
+
+              <p
+                className={cn(
+                  "text-base lg:text-lg font-jost font-medium",
+                  filterCategory.numberOfItems === 0 
+                  ? "text-muted"
+                  : "text-primary"
+                )}
+              >
+                {filterCategory.numberOfItems}
+              </p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
