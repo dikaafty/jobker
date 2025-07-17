c02bc2dc45a9428ed871280739bb476b22967dd3
Dika Afty <dikaafty@gmail.com>
2025-07-17 21:22:47 +0700

    Create filterCategories array and its values

--- src/pages/JobTrackerPage.tsx
+++ src/pages/JobTrackerPage.tsx
@@ -5,6 +5,16 @@ interface FilterCategoriesInterface {
   numberOfItems: number,
 }
 
+const filterCategories: FilterCategoriesInterface[] = [
+  { category: "BOOKMARK", numberOfItems: 0 },
+  { category: "APPLYING", numberOfItems: 0 },
+  { category: "APPLIED", numberOfItems: 0 },
+  { category: "INTERVIEWING", numberOfItems: 0 },
+  { category: "NEGOTIATING", numberOfItems: 0 },
+  { category: "ACCEPTED", numberOfItems: 0 },
+];
+
+
 const JobTrackerPage = () => {
   return (
     <>
