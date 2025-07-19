629e7550239f35c919bad50b5e5950120785b824
Dika Afty <dikaafty@gmail.com>
2025-07-19 14:40:56 +0700

    Create useAppSelector custom hook and export it

--- src/store/redux/hooks.ts
+++ src/store/redux/hooks.ts
@@ -1,2 +1,4 @@
 import { type TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
 import { type RootState, type AppDispatch } from "./store";
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
