a97e5ac885cb7e31929cc430b971ebd9fabc4d13
Dika Afty <dikaafty@gmail.com>
2025-07-19 14:15:53 +0700

    Delete filterCategories array

--- src/pages/JobTrackerPage.tsx
+++ src/pages/JobTrackerPage.tsx
@@ -2,16 +2,6 @@ import FilterCategoriesButton from "@/components/FilterCategoriesButton";
 import { useState } from "react";
 import { type FilterCategoriesInterface } from "@/interfaces/filterCategories";
 
-const filterCategories: FilterCategoriesInterface[] = [
-  { category: "BOOKMARKED", numberOfItems: 0 },
-  { category: "APPLYING", numberOfItems: 0 },
-  { category: "APPLIED", numberOfItems: 0 },
-  { category: "INTERVIEWING", numberOfItems: 0 },
-  { category: "NEGOTIATING", numberOfItems: 0 },
-  { category: "ACCEPTED", numberOfItems: 0 },
-];
-
-
 const JobTrackerPage = () => {
   return (
     <>
