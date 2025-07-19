0292518a130474cf2f4cafadb6b9ea914041182f
Dika Afty <dikaafty@gmail.com>
2025-07-19 20:18:10 +0700

    Import setJobTitle, setJobUrl, setJobLocation, setJobDescription, setCompanyName and setIsOpen reducers

--- src/components/jobTrackerPage/Dialog.tsx
+++ src/components/jobTrackerPage/Dialog.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/store/redux/hooks";
+import {
+  setJobTitle, setJobUrl, setJobLocation, setJobDescription, setCompanyName, setIsOpen
+} from "@/features/user/jobTrackerSlice";
 
 const Dialog = () => {
   return (
