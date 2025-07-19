ba19a48ccc6c6922664e0f8e9ffaae02566fe297
Dika Afty <dikaafty@gmail.com>
2025-07-19 19:39:30 +0700

    Create add job button container jsx and its classNames in AddJobButton component

--- src/components/AddJobButton.tsx
+++ src/components/AddJobButton.tsx
@@ -1,8 +1,10 @@
 const AddJobButton = () => {
   return (
-    <button>
+    <div className="w-full my-6 md:my-8 max-md:pl-4 flex md:justify-center items-center">
+      <button>
       
-    </button>
+      </button>
+    </div>
   )
 }
 
