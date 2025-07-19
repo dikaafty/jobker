2fa94b1d4923b3cff79ba4d40cc7027747eb1e83
Dika Afty <dikaafty@gmail.com>
2025-07-19 20:04:13 +0700

    Add onClick attr and its value to add job button jsx in AddJobButton component

--- src/components/AddJobButton.tsx
+++ src/components/AddJobButton.tsx
@@ -6,7 +6,10 @@ const AddJobButton = () => {
 
   return (
     <div className="w-full my-6 md:my-8 max-md:pl-4 flex md:justify-center items-center">
-      <button className="secondary-button">
+      <button 
+        className="secondary-button"
+        onClick={() => dispatch(setIsOpen())}  
+      >
         Add Job
       </button>
     </div>
