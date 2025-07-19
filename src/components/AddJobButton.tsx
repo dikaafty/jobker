f6840a84b3543e61d7e92d2ac7d3223e2d45dd9b
Dika Afty <dikaafty@gmail.com>
2025-07-19 19:40:18 +0700

    Add className and text content to add job button jsx in AddJobButton component

--- src/components/AddJobButton.tsx
+++ src/components/AddJobButton.tsx
@@ -1,8 +1,8 @@
 const AddJobButton = () => {
   return (
     <div className="w-full my-6 md:my-8 max-md:pl-4 flex md:justify-center items-center">
-      <button>
-      
+      <button className="secondary-button">
+        Add Job
       </button>
     </div>
   )
