a4136137c3f0c3e728b2a224f94a5059796d1d45
Dika Afty <dikaafty@gmail.com>
2025-07-19 14:13:54 +0700

    Render Provider in root

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { store } from './store/redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
