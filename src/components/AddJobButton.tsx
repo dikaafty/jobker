be5d9cea6e9a8535adb56673fdd05adea6028838
Dika Afty <dikaafty@gmail.com>
2025-07-19 19:58:35 +0700

    Setup dispatch in AddJobButton component

--- src/components/AddJobButton.tsx
+++ src/components/AddJobButton.tsx
@@ -2,6 +2,8 @@ import { setIsOpen } from "@/features/user/jobTrackerSlice";
 import { useAppDispatch } from "@/store/redux/hooks";
 
 const AddJobButton = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className="w-full my-6 md:my-8 max-md:pl-4 flex md:justify-center items-center">
       <button className="secondary-button">
