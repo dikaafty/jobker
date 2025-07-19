c0f22923067ba61259c3914d9b5270b1f0577fe9
Dika Afty <dikaafty@gmail.com>
2025-07-19 14:17:41 +0700

    Delete giving value of FilterCategoriesButton component's prop in its rendering in JobTrackerPage component

--- src/pages/JobTrackerPage.tsx
+++ src/pages/JobTrackerPage.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const JobTrackerPage = () => {
   return (
     <>
-      <FilterCategoriesButton filterCategories={filterJobCategories} />
+      <FilterCategoriesButton />
     </>
   )
 }
