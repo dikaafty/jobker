ffc9ddae74154c98126e8bb66782adf152ba91dc
Dika Afty <dikaafty@gmail.com>
2025-07-19 14:11:33 +0700

    Create store and its value

--- src/store/redux/store.ts
+++ src/store/redux/store.ts
@@ -1,2 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobTrackerReducer from "@/features/user/jobTrackerSlice";
+
+export const store = configureStore({
+  reducer: {
+    jobTracker: jobTrackerReducer,
+  }
+});
