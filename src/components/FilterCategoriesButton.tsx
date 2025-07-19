883f77b540e0fd9070366f5bb7fa2cc1e6de3122
Dika Afty <dikaafty@gmail.com>
2025-07-19 14:19:33 +0700

    Delete filterCategories prop of FilterCategoriesButton component

--- src/components/FilterCategoriesButton.tsx
+++ src/components/FilterCategoriesButton.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 
-const FilterCategoriesButton = ({ filterCategories }: FilterCategoriesButtonProps) => {
+const FilterCategoriesButton = () => {
   return (
     <div>
       {/* Desktop filter categories */}
