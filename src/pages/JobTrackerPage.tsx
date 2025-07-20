fb4c24d40f91b37da0090ccf7352d5e5b9546c9a
Dika Afty <dikaafty@gmail.com>
2025-07-20 10:13:49 +0700

    Import Dialog component

--- src/pages/JobTrackerPage.tsx
+++ src/pages/JobTrackerPage.tsx
@@ -1,5 +1,6 @@
 import FilterCategoriesButton from "@/components/jobTrackerPage/FilterCategoriesButton";
 import AddJobButton from "@/components/jobTrackerPage/AddJobButton";
+import Dialog from "@/components/jobTrackerPage/Dialog";
 import { useState } from "react";
 
 const JobTrackerPage = () => {
