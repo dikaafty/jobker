0b668675ccf35f0ea0a04a9ed9408fe5189f034f
Dika Afty <dikaafty@gmail.com>
2025-07-21 13:32:12 +0700

    Import JobTable component

--- src/pages/JobTrackerPage.tsx
+++ src/pages/JobTrackerPage.tsx
@@ -1,6 +1,7 @@
 import FilterCategoriesButton from "@/components/jobTrackerPage/FilterCategoriesButton";
 import AddJobButton from "@/components/jobTrackerPage/AddJobButton";
 import Dialog from "@/components/jobTrackerPage/Dialog";
+import JobTable from "@/components/jobTrackerPage/JobTable";
 import { useState } from "react";
 
 const JobTrackerPage = () => {
