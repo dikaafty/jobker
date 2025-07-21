9c40d5474f77322131fae644d2b6f14c76c2c530
Dika Afty <dikaafty@gmail.com>
2025-07-21 14:25:10 +0700

    Create jobTrackerStore variable and retreive all data from reduc store using useAppSelector custom hook in JobTable component

--- src/components/jobTrackerPage/JobTable.tsx
+++ src/components/jobTrackerPage/JobTable.tsx
@@ -1,6 +1,8 @@
 import { useAppSelector } from "@/store/redux/hooks";
 
 const JobTable = () => {
+  const jobTrackerStore = useAppSelector(state => state.jobTracker);
+
   return (
     <div>
       
