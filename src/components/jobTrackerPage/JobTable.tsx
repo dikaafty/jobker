e180acb3529cb213e4c409f7648650eb2a981b75
Dika Afty <dikaafty@gmail.com>
2025-07-22 10:48:49 +0700

    Create tablet job table and its items jsx in JobTable component

--- src/components/jobTrackerPage/JobTable.tsx
+++ src/components/jobTrackerPage/JobTable.tsx
@@ -6,7 +6,7 @@ const JobTable = () => {
   return (
     <div className="max-md:pl-4 flex md:justify-center">
 
-      {/* Desktop Job Table */}
+      {/* Desktop Table */}
 
       <table 
         className="font-jost w-[90%] order-separate border-spacing-0 rounded-lg overflow-hidden border 
@@ -35,6 +35,35 @@ const JobTable = () => {
                 <td className="py-3 border-b border-primary">{job.dateSaved}</td>
                 <td className="py-3 border-b border-primary">{job.dateApplied}</td>
                 <td className="pl-5 py-3 border-b border-primary">{job.deadline}</td>
+              </tr>
+            </tbody>
+          ))
+        }
+      </table>
+
+      {/* Tablet Job Table */}
+
+      <table 
+        className="font-jost w-[90%] order-separate border-spacing-0 rounded-lg overflow-hidden border 
+        border-primary hidden sm:table md:hidden"
+      >
+        <thead>
+          <tr className="bg-card font-semibold text-sm">
+            <td className="pl-5 py-3 border-b border-primary">Job Position</td>
+            <td className="py-3 border-b border-primary">Company</td>
+            <td className="py-3 border-b border-primary">Location</td>
+            <td className="py-3 border-b border-primary">Status</td>
+          </tr>
+        </thead>
+
+        {
+          jobTrackerStore.jobs.length > 0 && jobTrackerStore.jobs.map(job => (
+            <tbody key={job.companyName}>
+              <tr className="text-sm py-2">
+                <td className="pl-5 py-3 border-b border-primary">{job.jobTitle}</td>
+                <td className="py-3 border-b border-primary">{job.companyName}</td>
+                <td className="py-3 border-b border-primary">{job.jobLocation}</td>
+                <td className="py-3 border-b border-primary">{job.status}</td>
               </tr>
             </tbody>
           ))
