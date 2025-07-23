1fe0ebb70970def194ffe3f9bc458f720eac0523
Dika Afty <dikaafty@gmail.com>
2025-07-23 21:24:32 +0700

    Add span jsx in job url label in Dialog component

--- src/components/jobTrackerPage/Dialog.tsx
+++ src/components/jobTrackerPage/Dialog.tsx
@@ -84,6 +84,12 @@ const Dialog = () => {
                 className="dialog-label"
               >
                 Job Url
+                <span 
+                  className="text-xs sm:text-sm text-red-800 ml-1" 
+                  title="required field sign"
+                >
+                  *
+                </span>
               </label>
               <input
                 type="url"
