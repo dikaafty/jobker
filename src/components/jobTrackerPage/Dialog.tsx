099c7835dc59fbdb039f5aef3fb78a8b0ea1ffa2
Dika Afty <dikaafty@gmail.com>
2025-07-23 21:24:14 +0700

    Add span jsx in job title label in Dialog component

--- src/components/jobTrackerPage/Dialog.tsx
+++ src/components/jobTrackerPage/Dialog.tsx
@@ -62,6 +62,12 @@ const Dialog = () => {
                 className="dialog-label"
               >
                 Job Title
+                <span 
+                  className="text-xs sm:text-sm text-red-800 ml-1" 
+                  title="required field sign"
+                >
+                  *
+                </span>
               </label>
               <input
                 type="text"
