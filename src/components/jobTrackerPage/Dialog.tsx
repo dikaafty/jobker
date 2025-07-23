b2d8577917b97300fb3ddd333a19dd82e6fc8026
Dika Afty <dikaafty@gmail.com>
2025-07-23 19:29:56 +0700

    Import useState hook

--- src/components/jobTrackerPage/Dialog.tsx
+++ src/components/jobTrackerPage/Dialog.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/store/redux/hooks";
 import {
   setJobTitle, setJobUrl, setJobLocation, setJobDescription, setCompanyName, 
