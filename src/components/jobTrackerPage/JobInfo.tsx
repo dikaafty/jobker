7522a4611139f66280671bafeec33c689bc89aec
Dika Afty <dikaafty@gmail.com>
2025-07-27 13:33:31 +0700

    Create jobTrackerStore variable and retreive all data from redux store using useAppSelector custom hook in JobInfo component

--- src/components/jobTrackerPage/JobInfo.tsx
+++ src/components/jobTrackerPage/JobInfo.tsx
@@ -2,9 +2,11 @@ import { X } from "lucide-react";
 import { useAppSelector } from "@/store/redux/hooks";
 
 const JobInfo = () => {
+  const jobTrackerStore = useAppSelector(state => state.jobTracker);
+
   return (
     <div>
-      
+
     </div>
   )
 }
