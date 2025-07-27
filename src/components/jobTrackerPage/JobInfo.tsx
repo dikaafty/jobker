e94f48dcea1486fbd35dbb4774e68ab21446d943
Dika Afty <dikaafty@gmail.com>
2025-07-27 13:38:21 +0700

    Import setIsInfoOpen reducer

--- src/components/jobTrackerPage/JobInfo.tsx
+++ src/components/jobTrackerPage/JobInfo.tsx
@@ -1,5 +1,6 @@
 import { X } from "lucide-react";
 import { useAppSelector, useAppDispatch } from "@/store/redux/hooks";
+import { setIsInfoOpen } from "@/features/user/jobTrackerSlice";
 
 const JobInfo = () => {
   const jobTrackerStore = useAppSelector(state => state.jobTracker);
