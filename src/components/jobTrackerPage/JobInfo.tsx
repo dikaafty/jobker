1eb90a837ca5534913152b8fa63575f32ffc0a83
Dika Afty <dikaafty@gmail.com>
2025-07-27 13:34:44 +0700

    Setup dispatch using useAppDispatch custom hook in JobInfo component

--- src/components/jobTrackerPage/JobInfo.tsx
+++ src/components/jobTrackerPage/JobInfo.tsx
@@ -3,6 +3,7 @@ import { useAppSelector, useAppDispatch } from "@/store/redux/hooks";
 
 const JobInfo = () => {
   const jobTrackerStore = useAppSelector(state => state.jobTracker);
+  const dispatch = useAppDispatch();
 
   return (
     <div>
