74e96b000fb7649f2582528c5965d6e3c00b9a95
Dika Afty <dikaafty@gmail.com>
2025-07-28 10:09:30 +0700

    Call revertDateFormat function to wrap selected job deadline property in useEffect to set inital value of dialog fields in Dialog component

--- src/components/jobTrackerPage/Dialog.tsx
+++ src/components/jobTrackerPage/Dialog.tsx
@@ -92,7 +92,7 @@ const Dialog = () => {
       dispatch(setCompanyName(jobTrackerStore.selectedJob.companyName));
       dispatch(setStatus(jobTrackerStore.selectedJob.status));
       dispatch(setDateApplied(revertDateFormat(jobTrackerStore.selectedJob.dateApplied)));
-      dispatch(setDeadline(jobTrackerStore.selectedJob.deadline));
+      dispatch(setDeadline(revertDateFormat(jobTrackerStore.selectedJob.deadline)));
       dispatch(setJobDescription(jobTrackerStore.selectedJob.jobDescription));
     } else {
       dispatch(setJobTitle(""));
