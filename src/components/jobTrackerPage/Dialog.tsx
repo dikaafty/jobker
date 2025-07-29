0e65e83f4c1c4b0a05bfddf3bc7ad356702967b8
Dika Afty <dikaafty@gmail.com>
2025-07-29 13:47:18 +0700

    Import setIsEditing reducer

--- src/components/jobTrackerPage/Dialog.tsx
+++ src/components/jobTrackerPage/Dialog.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from "@/store/redux/hooks";
 import {
   setJobTitle, setJobUrl, setJobLocation, setJobDescription, setCompanyName, editJob,
   setIsOpen, addJob, setJobs, setStatus, setDateApplied, setDeadline, setSelectedJob,
+  setIsEditing
 } from "@/features/user/jobTrackerSlice";
 import { capitalize } from "@/lib/utils";
 import { Calendar } from "lucide-react";
