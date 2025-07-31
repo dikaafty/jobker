d6a288584b09b62920f2780575c58ed5027f6e89
Dika Afty <dikaafty@gmail.com>
2025-07-31 20:44:17 +0700

    Create jobkerGuide array and its items

--- src/components/aboutPage/AboutContent.tsx
+++ src/components/aboutPage/AboutContent.tsx
@@ -3,6 +3,21 @@ type JobkerGuide = {
   description: string
 }
 
+const jobkerGuide: JobkerGuide[] = [
+  {
+    title: "Add job applications with key details",
+    description: "Quickly log important information like company name, position title, job location, application date, and job description — all in one clean form."
+  },
+  {
+    title: "Track your progress with visual status updates",
+    description: `Easily manage where you are in the hiring process: from "Applied" to "Interview", "Negotiation", or "Accepted" —and filter jobs by status to stay on top of what matters.`
+  },
+  {
+    title: "Stay focused and never miss a follow-up",
+    description: "JOBKER helps you prioritize next steps, avoid forgotten applications, and maintain momentum by giving you a clear view of your entire job search journey."
+  }
+]
+
 const AboutContent = () => {
   return (
     <div>
