c426ab26063f0567fb4f93422b9fcce35fac4254
Dika Afty <dikaafty@gmail.com>
2025-07-31 14:13:46 +0700

    Create about hero and its items jsx in AboutHero component

--- src/components/aboutPage/AboutHero.tsx
+++ src/components/aboutPage/AboutHero.tsx
@@ -1,7 +1,19 @@
 const AboutHero = () => {
   return (
-    <div>
-      
+    <div className="bg-card flex flex-col justify-center items-center gap-8 font-jost py-20">
+      <div className="px-3 py-1 text-xs font-semibold border-1 border-secondary-foreground rounded-md">
+        ABOUT
+      </div>
+
+      <div className="text-5xl sm:text-6xl md:text-7xl text-center flex flex-col gap-3 font-semibold">
+        <p>Track smarter.</p>
+        <p>Land faster.</p>
+      </div>
+
+      <p className="text-base sm:text-lg md:text-xl text-center font-medium w-[85%] md:w-190">
+        JOBKER helps you organize, track, and stay focused on your job search — with a clean dashboard made 
+        for real people chasing real careers.
+      </p>
     </div>
   )
 }
