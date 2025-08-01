b9c96ca4d05ce596db981c0679430856bc4b1d90
Dika Afty <dikaafty@gmail.com>
2025-08-01 22:21:52 +0700

    Create transition function and its logic

--- src/lib/transition.ts
+++ src/lib/transition.ts
@@ -1,2 +1,59 @@
 import React from "react";
 import { motion } from "framer-motion";
+
+const transition = (OgComponent: React.FC): React.FC => {
+  const TransitionComponent = (props: React.ComponentProps<typeof OgComponent>) => {
+    return (
+      React.createElement(
+        React.Fragment,
+        null,
+        React.createElement(
+          OgComponent,
+          {...props}
+        ),
+        React.createElement(
+          motion.div,
+          {
+            className: "slide-in",
+            initial: { scaleX: 0 },
+            animate: { scaleX: 0 },
+            exit: { scaleX: 1 },
+            transition: { duration: 1, ease: [0.22, 1, 0.36, 1] }
+          }
+        ),
+        React.createElement(
+          motion.div,
+          {
+            className: "slide-out-one",
+            initial: { scaleX: 1 },
+            animate: { scaleX: 0 },
+            exit: { scaleX: 0 },
+            transition: { duration: 2, ease: [0.22, 1, 0.36, 1] }
+          }
+        ),
+        React.createElement(
+          motion.div,
+          {
+            className: "slide-out-two",
+            initial: { scaleX: 1 },
+            animate: { scaleX: 0 },
+            exit: { scaleX: 0 },
+            transition: { duration: 1.4, ease: [0.22, 1, 0.36, 1] }
+          }
+        ),
+        React.createElement(
+          motion.div,
+          {
+            className: "slide-out-one",
+            initial: { scaleX: 1 },
+            animate: { scaleX: 0 },
+            exit: { scaleX: 0 },
+            transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] }
+          }
+        ),
+      )
+    )
+  }
+
+  return TransitionComponent;
+}
