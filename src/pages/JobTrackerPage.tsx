deb5ec55e730505f22e240f9400f5b4183bc84cc
Dika Afty <dikaafty@gmail.com>
2025-08-01 22:35:30 +0700

    Delete useState hook import

--- src/pages/JobTrackerPage.tsx
+++ src/pages/JobTrackerPage.tsx
@@ -3,7 +3,6 @@ import AddJobButton from "@/components/jobTrackerPage/AddJobButton";
 import Dialog from "@/components/jobTrackerPage/Dialog";
 import JobTable from "@/components/jobTrackerPage/JobTable";
 import JobInfo from "@/components/jobTrackerPage/JobInfo";
-import { useState } from "react";
 import transition from "@/lib/transition";
 
 const JobTrackerPage = () => {
