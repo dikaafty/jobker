ae9da88206474f3d72cedb73bc141cec05610c96
Dika Afty <dikaafty@gmail.com>
2025-08-15 22:05:19 +0700

    Wrap classNames of add job button container jsx in AddJobButton component

--- src/components/jobTrackerPage/AddJobButton.tsx
+++ src/components/jobTrackerPage/AddJobButton.tsx
@@ -6,7 +6,10 @@ const AddJobButton = () => {
   const dispatch = useAppDispatch();
 
   return (
-    <div className="w-full my-6 md:my-8 max-md:pl-4 flex md:justify-center items-center">
+    <div 
+      className={cn(
+        "w-full my-6 md:my-8 max-md:pl-4 flex md:justify-center items-center"
+      )}>
       <button 
         className="secondary-button"
         onClick={() => dispatch(setIsOpen())}  
