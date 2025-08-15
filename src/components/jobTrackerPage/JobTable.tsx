9c7454681370622f95ca8df9eda62e26b83f9fd6
Dika Afty <dikaafty@gmail.com>
2025-08-15 22:03:27 +0700

    Create conditional to render or not all tables in JobTable component

--- src/components/jobTrackerPage/JobTable.tsx
+++ src/components/jobTrackerPage/JobTable.tsx
@@ -10,33 +10,135 @@ const JobTable = () => {
   .filter(job => jobTrackerStore.activeCategory === "all" || job.status === jobTrackerStore.activeCategory);
 
   return (
-    <div className="max-md:pl-4 flex md:justify-center pb-10 px-5">
+    filteredJobs.length > 0 && (
+      <div className="max-md:pl-4 flex md:justify-center pb-10 px-5">
 
-      {/* Desktop Table */}
+        {/* Desktop Table */}
 
-      <table 
-        className="font-jost min-w-180 border-separate border-spacing-0 rounded-lg overflow-hidden border 
-        border-primary hidden md:table"
-      >
-        <thead>
-          <tr className="bg-card font-semibold text-sm">
-            <td className="pl-5 py-3 border-b border-primary"></td>
-            <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Job Position</td>
-            <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Company</td>
-            <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Location</td>
-            <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Status</td>
-            <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Date Saved</td>
-            <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Date Applied</td>
-            <td className="pr-5 py-3 border-b border-primary">Deadline</td>
-          </tr>
-        </thead>
+        <table 
+          className="font-jost min-w-180 border-separate border-spacing-0 rounded-lg overflow-hidden border 
+          border-primary hidden md:table"
+        >
+          <thead>
+            <tr className="bg-card font-semibold text-sm">
+              <td className="pl-5 py-3 border-b border-primary"></td>
+              <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Job Position</td>
+              <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Company</td>
+              <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Location</td>
+              <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Status</td>
+              <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Date Saved</td>
+              <td className="max-[835px]:px-3 px-6 py-3 border-b border-primary">Date Applied</td>
+              <td className="pr-5 py-3 border-b border-primary">Deadline</td>
+            </tr>
+          </thead>
 
-        <tbody>
-        {
-          filteredJobs.length > 0 && filteredJobs.map(job => (
+          <tbody>
+          {
+            filteredJobs.length > 0 && filteredJobs.map(job => (
+                <tr 
+                  className="text-sm transition-colors duration-300 hover:bg-secondary"
+                  key={job.id}  
+                  onClick={() => {
+                    dispatch(setSelectedJob(job));
+                    dispatch(setIsInfoOpen());
+                  }}
+                >
+                  <td className="pl-5 py-3">
+                    <SquarePen 
+                      size={16} 
+                      className="cursor-pointer"
+                      onClick={(e) => {
+                        e.stopPropagation();
+
+                        dispatch(setIsOpen());
+                        dispatch(setIsEditing());
+                        dispatch(setSelectedJob(job));
+                      }}
+                    />
+                  </td>
+                  <td className="max-[835px]:px-3 px-6 py-3">{job.jobTitle}</td>
+                  <td className="max-[835px]:px-3 px-6 py-3">{job.companyName}</td>
+                  <td className="max-[835px]:px-3 px-6 py-3">{job.jobLocation}</td>
+                  <td className="max-[835px]:px-3 px-6 py-3">{job.status}</td>
+                  <td className="max-[835px]:px-3 px-6 py-3">{job.dateSaved}</td>
+                  <td className="max-[835px]:px-3 px-6 py-3">{job.dateApplied}</td>
+                  <td className="pr-5 py-3">{job.deadline}</td>
+                </tr>
+            ))
+          }
+          </tbody>
+        </table>
+
+        {/* Tablet Job Table */}
+
+        <table 
+          className="font-jost w-full border-separate border-spacing-0 rounded-lg overflow-hidden border 
+          border-primary hidden sm:table md:hidden"
+        >
+          <thead>
+            <tr className="bg-card font-bold text-sm">
+              <td className="pl-5 py-3 border-b border-primary"></td>
+              <td className="py-3 border-b border-primary">Job Position</td>
+              <td className="py-3 border-b border-primary">Company</td>
+              <td className="py-3 border-b border-primary">Location</td>
+              <td className="pr-5 py-3 border-b border-primary">Status</td>
+            </tr>
+          </thead>
+
+          <tbody>
+          {
+            filteredJobs.length > 0 && filteredJobs.map(job => (
               <tr 
-                className="text-sm transition-colors duration-300 hover:bg-secondary"
-                key={job.id}  
+                className="text-sm py-2 transition-colors duration-300 hover:bg-secondary"
+                key={job.id}
+                onClick={() => {
+                  dispatch(setSelectedJob(job));
+                  dispatch(setIsInfoOpen());
+                }}
+              >
+                <td className="pl-5 pr-1 py-3">
+                  <SquarePen 
+                    size={16} 
+                    className="cursor-pointer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+
+                      dispatch(setIsOpen());
+                      dispatch(setIsEditing());
+                      dispatch(setSelectedJob(job));
+                    }}
+                  />
+                </td>
+                <td className="py-3">{job.jobTitle}</td>
+                <td className="py-3">{job.companyName}</td>
+                <td className="py-3">{job.jobLocation}</td>
+                <td className="py-3">{job.status}</td>
+              </tr>
+            ))
+          }
+          </tbody>
+        </table>
+
+        {/* Mobile Job Table */}
+
+        <table 
+          className="font-jost w-full border-separate border-spacing-0 rounded-lg overflow-hidden border 
+          border-primary table sm:hidden"
+        >
+          <thead>
+            <tr className="bg-card font-semibold text-sm">
+              <td className="pl-5 py-3 border-b border-primary"></td>
+              <td className="py-3 border-b border-primary">Job Position</td>
+              <td className="py-3 border-b border-primary">Status</td>
+            </tr>
+          </thead>
+
+          <tbody>
+          {
+            filteredJobs.length > 0 && filteredJobs.map(job => (
+              <tr 
+                className="text-sm py-2 transition-colors duration-300 hover:bg-secondary"
+                key={job.id}
                 onClick={() => {
                   dispatch(setSelectedJob(job));
                   dispatch(setIsInfoOpen());
@@ -55,115 +157,15 @@ const JobTable = () => {
                     }}
                   />
                 </td>
-                <td className="max-[835px]:px-3 px-6 py-3">{job.jobTitle}</td>
-                <td className="max-[835px]:px-3 px-6 py-3">{job.companyName}</td>
-                <td className="max-[835px]:px-3 px-6 py-3">{job.jobLocation}</td>
-                <td className="max-[835px]:px-3 px-6 py-3">{job.status}</td>
-                <td className="max-[835px]:px-3 px-6 py-3">{job.dateSaved}</td>
-                <td className="max-[835px]:px-3 px-6 py-3">{job.dateApplied}</td>
-                <td className="pr-5 py-3">{job.deadline}</td>
+                <td className="py-3">{job.jobTitle}</td>
+                <td className="py-3">{job.status}</td>
               </tr>
-          ))
-        }
-        </tbody>
-      </table>
-
-      {/* Tablet Job Table */}
-
-      <table 
-        className="font-jost w-full border-separate border-spacing-0 rounded-lg overflow-hidden border 
-        border-primary hidden sm:table md:hidden"
-      >
-        <thead>
-          <tr className="bg-card font-bold text-sm">
-            <td className="pl-5 py-3 border-b border-primary"></td>
-            <td className="py-3 border-b border-primary">Job Position</td>
-            <td className="py-3 border-b border-primary">Company</td>
-            <td className="py-3 border-b border-primary">Location</td>
-            <td className="pr-5 py-3 border-b border-primary">Status</td>
-          </tr>
-        </thead>
-
-        <tbody>
-        {
-          filteredJobs.length > 0 && filteredJobs.map(job => (
-            <tr 
-              className="text-sm py-2 transition-colors duration-300 hover:bg-secondary"
-              key={job.id}
-              onClick={() => {
-                dispatch(setSelectedJob(job));
-                dispatch(setIsInfoOpen());
-              }}
-            >
-              <td className="pl-5 pr-1 py-3">
-                <SquarePen 
-                  size={16} 
-                  className="cursor-pointer"
-                  onClick={(e) => {
-                    e.stopPropagation();
-
-                    dispatch(setIsOpen());
-                    dispatch(setIsEditing());
-                    dispatch(setSelectedJob(job));
-                  }}
-                />
-              </td>
-              <td className="py-3">{job.jobTitle}</td>
-              <td className="py-3">{job.companyName}</td>
-              <td className="py-3">{job.jobLocation}</td>
-              <td className="py-3">{job.status}</td>
-            </tr>
-          ))
-        }
-        </tbody>
-      </table>
-
-      {/* Mobile Job Table */}
-
-      <table 
-        className="font-jost w-full border-separate border-spacing-0 rounded-lg overflow-hidden border 
-        border-primary table sm:hidden"
-      >
-        <thead>
-          <tr className="bg-card font-semibold text-sm">
-            <td className="pl-5 py-3 border-b border-primary"></td>
-            <td className="py-3 border-b border-primary">Job Position</td>
-            <td className="py-3 border-b border-primary">Status</td>
-          </tr>
-        </thead>
-
-        <tbody>
-        {
-          filteredJobs.length > 0 && filteredJobs.map(job => (
-            <tr 
-              className="text-sm py-2 transition-colors duration-300 hover:bg-secondary"
-              key={job.id}
-              onClick={() => {
-                dispatch(setSelectedJob(job));
-                dispatch(setIsInfoOpen());
-              }}
-            >
-              <td className="pl-5 py-3">
-                <SquarePen 
-                  size={16} 
-                  className="cursor-pointer"
-                  onClick={(e) => {
-                    e.stopPropagation();
-
-                    dispatch(setIsOpen());
-                    dispatch(setIsEditing());
-                    dispatch(setSelectedJob(job));
-                  }}
-                />
-              </td>
-              <td className="py-3">{job.jobTitle}</td>
-              <td className="py-3">{job.status}</td>
-            </tr>
-          ))
-        }
-        </tbody>
-      </table>
-    </div>
+            ))
+          }
+          </tbody>
+        </table>
+      </div>
+    )
   )
 }
 
