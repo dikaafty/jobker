dc2c113639493bdbb30937b951cf23f54fea4350
Dika Afty <dikaafty@gmail.com>
2025-08-23 04:06:43 +0700

    Delete renderHeroSection function and its invoketion

--- src/components/homepage/__test__/HeroSection.test.tsx
+++ src/components/homepage/__test__/HeroSection.test.tsx
@@ -2,18 +2,8 @@ import HeroSection from "../HeroSection";
 import { render, screen, userEvent } from "@/lib/test-utils";
 import { MemoryRouter } from "react-router-dom";
 
-const renderHeroSection = (): void => {
-  render(
-    <MemoryRouter>
-      <HeroSection />
-    </MemoryRouter>
-  );
-}
-
 describe("HeroSection Component", () => {
   test("renders HeroSection with correct elements", () => {
-    renderHeroSection();
-
     expect(screen.getByText("YOUR CAREER")).toBeInTheDocument();
     expect(screen.getByText("DASHBOARD STARTS HERE")).toBeInTheDocument();
     expect(screen.getByText("Organize your applications, monitor progress, and take control of your career."))
