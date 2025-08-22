e5add67e4a78621aa43f2aa44f4e32ad954876f2
Dika Afty <dikaafty@gmail.com>
2025-08-23 03:53:32 +0700

    Change import source of render function and screen object

--- src/components/homepage/__test__/HeroSection.test.tsx
+++ src/components/homepage/__test__/HeroSection.test.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "../HeroSection";
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@/lib/test-utils";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 
