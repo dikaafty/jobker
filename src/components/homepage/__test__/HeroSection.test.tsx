317d52a07a970cd5365e5db54ed469fb3223e076
Dika Afty <dikaafty@gmail.com>
2025-08-23 01:18:55 +0700

    Create renderHeroSection function and its logic

--- src/components/homepage/__test__/HeroSection.test.tsx
+++ src/components/homepage/__test__/HeroSection.test.tsx
@@ -3,6 +3,14 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 
+const renderHeroSection = (): void => {
+  render(
+    <MemoryRouter>
+      <HeroSection />
+    </MemoryRouter>
+  );
+}
+
 describe("HeroSection Component", () => {
   
 });
