1d094d0650603e39601e2a52850d6a7fc1cde64d
Dika Afty <dikaafty@gmail.com>
2025-08-23 01:20:24 +0700

    Create test for checking HeroSection component rendered with correct elements

--- src/components/homepage/__test__/HeroSection.test.tsx
+++ src/components/homepage/__test__/HeroSection.test.tsx
@@ -12,5 +12,14 @@ const renderHeroSection = (): void => {
 }
 
 describe("HeroSection Component", () => {
-  
+  test("renders HeroSection with correct elements", () => {
+    renderHeroSection();
+
+    expect(screen.getByText("YOUR CAREER")).toBeInTheDocument();
+    expect(screen.getByText("DASHBOARD STARTS HERE")).toBeInTheDocument();
+    expect(screen.getByText("Organize your applications, monitor progress, and take control of your career."))
+    .toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Get Started" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "About Us" })).toBeInTheDocument();
+  });
 });
