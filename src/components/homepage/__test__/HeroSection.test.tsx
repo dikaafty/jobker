354d3b16c439b368dcbbfe7d3719628526320105
Dika Afty <dikaafty@gmail.com>
2025-08-23 01:18:35 +0700

    Import MemoryRouter component

--- src/components/homepage/__test__/HeroSection.test.tsx
+++ src/components/homepage/__test__/HeroSection.test.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "../HeroSection";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 
 describe("HeroSection Component", () => {
   
