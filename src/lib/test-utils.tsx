d8b0be2bb98214208ca66963e46ac2280049e667
Dika Afty <dikaafty@gmail.com>
2025-08-23 13:56:23 +0700

    Import ReactElement interface

--- src/lib/test-utils.tsx
+++ src/lib/test-utils.tsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import { MemoryRouter, type MemoryRouterProps } from "react-router-dom";
 import { render, type RenderOptions } from "@testing-library/react";
+import { type ReactElement } from "react";
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
