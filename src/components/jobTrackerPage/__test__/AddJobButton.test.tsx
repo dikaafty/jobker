02fc6b03a13703efc78d672d7cc140627ab0666b
Dika Afty <dikaafty@gmail.com>
2025-08-23 22:28:48 +0700

    Create test for checking that AddButton component rendered with the correct elements

--- src/components/jobTrackerPage/__test__/AddJobButton.test.tsx
+++ src/components/jobTrackerPage/__test__/AddJobButton.test.tsx
@@ -2,5 +2,19 @@ import AddJobButton from "../AddJobButton";
 import { render, screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("AddJobButton component", () => {
-  
+  test("renders AddJobButton with correct elements", () => {
+    renderWithProvider(
+      <AddJobButton />,
+      {
+        preloadedState: {
+          jobTracker: {
+            jobs: [],
+            activeCategory: "all"
+          }
+        }
+      }
+    );
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
 });
