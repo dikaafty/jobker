69af36e8960e027eecbdd8023d28e8550aa9cd44
Dika Afty <dikaafty@gmail.com>
2025-08-23 23:10:54 +0700

    Create test for checking that Dialog component will not be rendered if isOpen state's value is false

--- src/components/jobTrackerPage/__test__/Dialog.test.tsx
+++ src/components/jobTrackerPage/__test__/Dialog.test.tsx
@@ -2,5 +2,20 @@ import Dialog from "../Dialog";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("Dialog component", () => {
-  
+  test("Dialog component will not be rendered if isOpen state's value is false", () => {
+    renderWithProvider(
+      <Dialog />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: false,
+            selectedJob: null,
+            jobs: [],
+          }
+        }
+      }
+    );
+
+    expect(screen.queryByTestId("dialog")).not.toBeInTheDocument();
+  });
 });
