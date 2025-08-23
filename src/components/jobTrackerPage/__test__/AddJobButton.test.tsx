73fe1c6b55f87ce053bc15fad6b1f48b8375ebb2
Dika Afty <dikaafty@gmail.com>
2025-08-23 22:38:59 +0700

    Import setIsOpen reducer

--- src/components/jobTrackerPage/__test__/AddJobButton.test.tsx
+++ src/components/jobTrackerPage/__test__/AddJobButton.test.tsx
@@ -1,5 +1,6 @@
 import AddJobButton from "../AddJobButton";
 import { render, screen, userEvent, renderWithProvider } from "@/lib/test-utils";
+import { setIsOpen } from "@/features/user/jobTrackerSlice";
 
 describe("AddJobButton component", () => {
   test("renders AddJobButton with correct elements", () => {
