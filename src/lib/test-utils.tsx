ed3603954ae7fb1d2a7c77f99f55874096f84345
Dika Afty <dikaafty@gmail.com>
2025-08-23 14:00:47 +0700

    Create renderWithRouter function and its logic

--- src/lib/test-utils.tsx
+++ src/lib/test-utils.tsx
@@ -2,5 +2,24 @@ import "@testing-library/jest-dom";
 import { MemoryRouter, type MemoryRouterProps } from "react-router-dom";
 import { render, type RenderOptions } from "@testing-library/react";
 import { type ReactElement } from "react";
+
+const renderWithRouter = (
+  ui: ReactElement,
+  renderProps?: MemoryRouterProps,
+  renderOptions?: RenderOptions
+) => {
+  return render(
+    ui,
+    {
+      wrapper: ({ children }) => (
+        <MemoryRouter {...renderProps}>
+          {children}
+        </MemoryRouter>
+      ),
+      ...renderOptions,
+    }
+  );
+}
+
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
