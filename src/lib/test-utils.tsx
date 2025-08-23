61181ba368c8d804cdfaba84e3b7e1328abe4fb6
Dika Afty <dikaafty@gmail.com>
2025-08-23 13:54:47 +0700

    Import MemoryRouter component and MemoryRouterProps interface

--- src/lib/test-utils.tsx
+++ src/lib/test-utils.tsx
@@ -1,3 +1,4 @@
 import "@testing-library/jest-dom";
+import { MemoryRouter, type MemoryRouterProps } from "react-router-dom";
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
