f5bc135e4770fc3b6d3eeb95a4253303dc020a09
Dika Afty <dikaafty@gmail.com>
2025-08-24 13:21:32 +0700

    Create a test for checking that Dialog will be rendered with the correct elements if isOpen state's value is true

--- src/components/jobTrackerPage/__test__/Dialog.test.tsx
+++ src/components/jobTrackerPage/__test__/Dialog.test.tsx
@@ -18,4 +18,43 @@ describe("Dialog component", () => {
 
     expect(screen.queryByTestId("dialog")).not.toBeInTheDocument();
   });
+
+  test("renders Dialog with correct elements if isOpen state's value is true", () => {
+    renderWithProvider(
+      <Dialog />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: true,
+            selectedJob: null,
+            jobs: [],
+            filterCategories: [],
+          }
+        }
+      }
+    );
+
+    expect(screen.getByTestId("dialog")).toBeInTheDocument();
+    expect(screen.getByText(/add new job/i)).toBeInTheDocument();
+    expect(screen.getAllByText("*")).toHaveLength(5);
+
+    expect(screen.getByText(/job title/i)).toBeInTheDocument();
+    expect(screen.getByText(/job url/i)).toBeInTheDocument();
+    expect(screen.getByText(/job location/i)).toBeInTheDocument();
+    expect(screen.getByText(/company name/i)).toBeInTheDocument();
+    expect(screen.getByText(/status/i)).toBeInTheDocument();
+    expect(screen.getByText(/deadline/i)).toBeInTheDocument();
+    expect(screen.getByText(/job description/i)).toBeInTheDocument();
+
+    expect(screen.getByLabelText(/job title/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/job url/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/job location/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/company name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/status/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/deadline/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/job description/i)).toBeInTheDocument();
+
+    expect(screen.getByRole("button", { name: /cancel/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /save job/i })).toBeInTheDocument();
+  });
 });
