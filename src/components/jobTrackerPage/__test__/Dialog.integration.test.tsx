f1aed63de7fe2ac9cf3761535541230d5400f7f4
Dika Afty <dikaafty@gmail.com>
2025-08-24 23:55:04 +0700

    Create a test to check that Status state from Redux stroe will be updated when Status select value changes

--- src/components/jobTrackerPage/__test__/Dialog.integration.test.tsx
+++ src/components/jobTrackerPage/__test__/Dialog.integration.test.tsx
@@ -2,5 +2,31 @@ import Dialog from "../Dialog";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("Dialog component", () => {
-  
+  test("updates Redux store Status state when Status select value changes", async () => {
+    const { store } = renderWithProvider(
+      <Dialog />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: true,
+            selectedJob: null,
+            jobs: [],
+            filterCategories: [
+              { category: "BOOKMARKED" },
+              { category: "APPLYING" },
+            ],
+          }
+        },
+        storeType: "integration"
+      }
+    );
+    
+    const user = userEvent.setup();
+
+    expect(store.getState().jobTracker.status).toBe("Bookmarked");
+
+    await user.selectOptions(screen.getByLabelText(/status/i), "Applying");
+
+    expect(store.getState().jobTracker.status).toBe("Applying");
+  });
 });
