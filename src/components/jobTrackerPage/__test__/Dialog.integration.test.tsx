5fd7044560820b578f275d01e21f4f4c399a9b17
Dika Afty <dikaafty@gmail.com>
2025-08-25 00:01:00 +0700

    Create a test to check that isOpen state from Redux store will be updated and Dialog will be closed when Cancel button is clicked

--- src/components/jobTrackerPage/__test__/Dialog.integration.test.tsx
+++ src/components/jobTrackerPage/__test__/Dialog.integration.test.tsx
@@ -29,4 +29,34 @@ describe("Dialog component", () => {
 
     expect(store.getState().jobTracker.status).toBe("Applying");
   });
+
+  test("updates Redux store isOpen state and closes the Dialog when Cancel button clicked", async () => {
+     const { store } = renderWithProvider(
+      <Dialog />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: true,
+            selectedJob: null,
+            jobs: [],
+            filterCategories: [
+              { category: "BOOKMARKED" },
+              { category: "APPLYING" },
+            ],
+          }
+        },
+        storeType: "integration"
+      }
+    );
+
+    const user = userEvent.setup();
+
+    expect(screen.getByTestId("dialog")).toBeInTheDocument();
+    expect(store.getState().jobTracker.isOpen).toBe(true);
+
+    await user.click(screen.getByRole("button", { name: /cancel/i }));
+
+    expect(screen.queryByTestId("dialog")).not.toBeInTheDocument();
+    expect(store.getState().jobTracker.isOpen).toBe(false);
+  });
 });
