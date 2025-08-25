0de43a58bb824fff1a876f3c134cbb9271e406ed
Dika Afty <dikaafty@gmail.com>
2025-08-26 00:53:15 +0700

    Create a test to check that desktop job table will be rendered with the correct elements

--- src/components/jobTrackerPage/__test__/JobTable.unit.test.tsx
+++ src/components/jobTrackerPage/__test__/JobTable.unit.test.tsx
@@ -44,4 +44,48 @@ describe("JobTable component", () => {
     expect(screen.queryByText("Your dashboard’s a bit lonely. Add your first job and get tracking!")).not.toBeInTheDocument();
     expect(screen.getAllByRole("table")).toHaveLength(3);
   });
+
+  test("renders desktop job table with correct elements", () => {
+    renderWithProvider(
+      <JobTable />,
+      {
+        preloadedState: {
+          jobTracker: {
+            jobs: [
+              {
+                id: 1,
+                jobTitle: "Front End Developer",
+                jobLocation: "Remote",
+                companyName: "Link AI",
+                status: "Applied",
+                dateSaved: "14/08/2025",
+                dateApplied: "15/08/2025",
+                deadline: "25/08/2025",
+              }
+            ],
+            activeCategory: "all",
+          }
+        }
+      }
+    );
+
+    expect(screen.getAllByRole("table")[0]).toBeInTheDocument();
+    
+    expect(screen.getAllByText(/job position/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/company/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/location/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/status/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/date saved/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/date applied/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/deadline/i)[0]).toBeInTheDocument();
+
+    expect(screen.getAllByRole("button", { name: /edit/i })[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/front end developer/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/link ai/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/applied/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/remote/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText("14/08/2025")[0]).toBeInTheDocument();
+    expect(screen.getAllByText("15/08/2025")[0]).toBeInTheDocument();
+    expect(screen.getAllByText("25/08/2025")[0]).toBeInTheDocument();
+  });
 });
