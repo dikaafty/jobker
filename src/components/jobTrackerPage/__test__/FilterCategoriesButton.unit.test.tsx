3ca3987c16a52bdca2d5efcadebf6b0c8d7edeea
Dika Afty <dikaafty@gmail.com>
2025-08-25 14:31:44 +0700

    Create a test to check that FilterCategoriesButton component rendered with the correct elements

--- src/components/jobTrackerPage/__test__/FilterCategoriesButton.unit.test.tsx
+++ src/components/jobTrackerPage/__test__/FilterCategoriesButton.unit.test.tsx
@@ -2,5 +2,33 @@ import FilterCategoriesButton from "../FilterCategoriesButton";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("FilterCategoriesButton component", () => {
-  
+  test("renders FilterCategoriesButton component with correct elements", () => {
+    renderWithProvider(
+      <FilterCategoriesButton />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: true,
+            selectedJob: null,
+            jobs: [],
+            filterCategories: [
+              { id: 1, category: "BOOKMARKED", numberOfItems: 0 },
+              { id: 2, category: "APPLYING", numberOfItems: 0 },
+              { id: 3, category: "APPLIED", numberOfItems: 0 },
+              { id: 4, category: "INTERVIEWING", numberOfItems: 0 },
+              { id: 5, category: "NEGOTIATING", numberOfItems: 0 },
+              { id: 6, category: "ACCEPTED", numberOfItems: 0 },
+            ],
+          }
+        }
+      }
+    );
+
+    expect(screen.getAllByText(/bookmarked/i)).toHaveLength(2);
+    expect(screen.getAllByText(/applying/i)).toHaveLength(2);
+    expect(screen.getAllByText(/applied/i)).toHaveLength(2);
+    expect(screen.getAllByText(/interviewing/i)).toHaveLength(2);
+    expect(screen.getAllByText(/negotiating/i)).toHaveLength(2);
+    expect(screen.getAllByText(/accepted/i)).toHaveLength(2);
+  });
 });
