2592f02750a209bffdcaaeb0def37f3ab2ca8726
Dika Afty <dikaafty@gmail.com>
2025-08-25 14:55:13 +0700

    Create a test to check that bg-secondary classname will be toggled on desktop filter category button when clicked and has item

--- src/components/jobTrackerPage/__test__/FilterCategoriesButton.unit.test.tsx
+++ src/components/jobTrackerPage/__test__/FilterCategoriesButton.unit.test.tsx
@@ -31,4 +31,34 @@ describe("FilterCategoriesButton component", () => {
     expect(screen.getAllByText(/negotiating/i)).toHaveLength(2);
     expect(screen.getAllByText(/accepted/i)).toHaveLength(2);
   });
+
+  test("toggles bg-secondary on desktop filter category button when clicked and has item", async () => {
+    renderWithProvider(
+      <FilterCategoriesButton />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: true,
+            selectedJob: null,
+            jobs: [],
+            filterCategories: [
+              { id: 1, category: "BOOKMARKED", numberOfItems: 1 },
+            ],
+          }
+        }
+      }
+    );
+
+    const user = userEvent.setup();
+
+    expect(screen.getByTestId(/bookmarked button/i)).not.toHaveClass("bg-secondary");
+
+    await user.click(screen.getByTestId(/bookmarked button/i));
+
+    expect(screen.getByTestId(/bookmarked button/i)).toHaveClass("bg-secondary");
+
+    await user.click(screen.getByTestId(/bookmarked button/i));
+
+    expect(screen.getByTestId(/bookmarked button/i)).not.toHaveClass("bg-secondary");
+  });
 });
