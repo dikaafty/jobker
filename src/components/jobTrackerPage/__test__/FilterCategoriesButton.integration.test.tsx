e3c9eec2ff6643f63afeef6c652fe7b1e4bd7c26
Dika Afty <dikaafty@gmail.com>
2025-08-25 20:14:42 +0700

    Create a test to chech that activeCategory state from Redux store will be updated when mobile filter category select options selected

--- src/components/jobTrackerPage/__test__/FilterCategoriesButton.integration.test.tsx
+++ src/components/jobTrackerPage/__test__/FilterCategoriesButton.integration.test.tsx
@@ -2,5 +2,31 @@ import FilterCategoriesButton from "../FilterCategoriesButton";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("FilterCategoriesButton component", () => {
-  
+  test("updates Redux store activeCategory state when mobile filter category select options selected", async () => {
+    const { store } = renderWithProvider(
+      <FilterCategoriesButton />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: true,
+            selectedJob: null,
+            jobs: [],
+            filterCategories: [
+              { id: 1, category: "BOOKMARKED", numberOfItems: 1 },
+            ],
+            activeCategory: "all",
+          }
+        },
+        storeType: "integration"
+      }
+    );
+
+    const user = userEvent.setup();
+
+    expect(store.getState().jobTracker.activeCategory).toBe("all");
+
+    await user.selectOptions(screen.getByRole("combobox"), "Bookmarked");
+
+    expect(store.getState().jobTracker.activeCategory).toBe("Bookmarked");
+  });
 });
