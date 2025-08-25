7f5d66e01059bd5995b4b26b59b49a956eeb8be7
Dika Afty <dikaafty@gmail.com>
2025-08-25 21:00:10 +0700

    Create a test to check that activeCategory state from Redux store will be updated when filter category button clicked

--- src/components/jobTrackerPage/__test__/FilterCategoriesButton.integration.test.tsx
+++ src/components/jobTrackerPage/__test__/FilterCategoriesButton.integration.test.tsx
@@ -2,6 +2,37 @@ import FilterCategoriesButton from "../FilterCategoriesButton";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("FilterCategoriesButton component", () => {
+  test("updates Redux store activeCategory state when desktop filter category button clicked", async () => {
+    const { store } = renderWithProvider(
+      <FilterCategoriesButton />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isOpen: true,
+            selectedJob: null,
+            jobs: [
+              { status: "Bookmarked" },
+            ],
+            activeCategory: "all",
+            filterCategories: [
+              { id: 1, category: "BOOKMARKED", numberOfItems: 0 },
+            ],
+          }
+        },
+        storeType: "integration"
+      }
+    );
+
+    const user = userEvent.setup();
+
+    expect(store.getState().jobTracker.activeCategory).toBe("all");
+    expect(screen.getByTestId(/bookmarked button/i)).toBeInTheDocument();
+
+    await user.click(screen.getByTestId(/bookmarked button/i));
+
+    expect(store.getState().jobTracker.activeCategory).toBe("Bookmarked");
+  });
+
   test("updates Redux store activeCategory state when mobile filter category select options selected", async () => {
     const { store } = renderWithProvider(
       <FilterCategoriesButton />,
