2e8f35bf50b7ce4048fa8208ebe857402e4b27b4
Dika Afty <dikaafty@gmail.com>
2025-08-25 23:56:42 +0700

    Create a test to check that JobTable component will render CTA instead of job table when jobs state has empty array value

--- src/components/jobTrackerPage/__test__/JobTable.unit.test.tsx
+++ src/components/jobTrackerPage/__test__/JobTable.unit.test.tsx
@@ -2,5 +2,20 @@ import JobTable from "../JobTable";
 import { screen, renderWithProvider } from "@/lib/test-utils";
 
 describe("JobTable component", () => {
-  
+  test("renders CTA instead of job table when has no jobs", () => {
+    renderWithProvider(
+      <JobTable />,
+      {
+        preloadedState: {
+          jobTracker: {
+            jobs: [],
+            activeCategory: "all",
+          }
+        }
+      }
+    );
+
+    expect(screen.getByText("Your dashboard’s a bit lonely. Add your first job and get tracking!")).toBeInTheDocument();
+    expect(screen.queryAllByRole("table")).not.toHaveLength(3);
+  });
 });
