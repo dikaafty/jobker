28974f2c440558ab4db647f6ea97691490e87158
Dika Afty <dikaafty@gmail.com>
2025-08-26 00:51:18 +0700

    Create a test to check that JobTable component will render job table instead of CTA when jobs state has job items inside of its array value

--- src/components/jobTrackerPage/__test__/JobTable.unit.test.tsx
+++ src/components/jobTrackerPage/__test__/JobTable.unit.test.tsx
@@ -18,4 +18,30 @@ describe("JobTable component", () => {
     expect(screen.getByText("Your dashboard’s a bit lonely. Add your first job and get tracking!")).toBeInTheDocument();
     expect(screen.queryAllByRole("table")).not.toHaveLength(3);
   });
+
+  test("renders job table instead of CTA when has jobs", () => {
+    renderWithProvider(
+      <JobTable />,
+      {
+        preloadedState: {
+          jobTracker: {
+            jobs: [
+              {
+                id: 1,
+                jobTitle: "Front End Developer",
+                jobUrl: "https://www.remoterocketship.com/jobs/frontend-developer",
+                jobLocation: "Remote",
+                companyName: "Link AI",
+                status: "Applied",
+              }
+            ],
+            activeCategory: "all",
+          }
+        }
+      }
+    );
+
+    expect(screen.queryByText("Your dashboard’s a bit lonely. Add your first job and get tracking!")).not.toBeInTheDocument();
+    expect(screen.getAllByRole("table")).toHaveLength(3);
+  });
 });
