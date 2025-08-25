1d4a576107528297bf0c3ca089e80df601a0180d
Dika Afty <dikaafty@gmail.com>
2025-08-25 21:24:02 +0700

    Create a test to check that JobInfo component will not be rendered if isInfoOpen state's value is false

--- src/components/jobTrackerPage/__test__/JobInfo.unit.test.tsx
+++ src/components/jobTrackerPage/__test__/JobInfo.unit.test.tsx
@@ -2,5 +2,19 @@ import JobInfo from "../JobInfo";
 import { screen, renderWithProvider } from "@/lib/test-utils";
 
 describe("JobInfo component", () => {
-  
+  test("JobInfo component will not be rendered if isInfoOpen state's value is false", () => {
+    renderWithProvider(
+      <JobInfo />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isInfoOpen: false,
+            selectedJob: null,
+          }
+        }
+      }
+    );
+
+    expect(screen.queryByTestId(/job info/i)).not.toBeInTheDocument();
+  });
 });
