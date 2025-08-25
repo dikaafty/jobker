f22c9df143a52b9e0ef5b484d36397edacee53c5
Dika Afty <dikaafty@gmail.com>
2025-08-25 21:48:03 +0700

    Create a test to check that JobInfo will be rendered with the correct element if isInfoOpen state's value is true

--- src/components/jobTrackerPage/__test__/JobInfo.unit.test.tsx
+++ src/components/jobTrackerPage/__test__/JobInfo.unit.test.tsx
@@ -17,4 +17,48 @@ describe("JobInfo component", () => {
 
     expect(screen.queryByTestId(/job info/i)).not.toBeInTheDocument();
   });
+
+  test("renders JobInfo with correct element if isInfoOpen state's value is true", () => {
+    renderWithProvider(
+      <JobInfo />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isInfoOpen: true,
+            selectedJob: {
+              jobTitle: "Front-End Developer",
+              jobUrl: "https://www.remoterocketship.com/jobs/frontend-developer",
+              jobLocation: "Remote",
+              companyName: "Link AI",
+              status: "Bookmarked",
+              dateSaved: "04/08/2025",
+              dateApplied: "05/08/2025",
+              deadline: "25/08/2025",
+              jobDescription: "Some description..."
+            },
+          }
+        }
+      }
+    );
+
+    expect(screen.queryByTestId(/job info/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/job url/i)).toBeInTheDocument();
+    expect(screen.getByText(/date saved/i)).toBeInTheDocument();
+    expect(screen.getByText(/date applied/i)).toBeInTheDocument();
+    expect(screen.getByText(/deadline/i)).toBeInTheDocument();
+    expect(screen.getByText(/job description/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/front-end developer/i)).toBeInTheDocument();
+    expect(screen.getByText(/link ai/i)).toBeInTheDocument();
+    expect(screen.getByText(/bookmarked/i)).toBeInTheDocument();
+    expect(screen.getByText(/some description.../i)).toBeInTheDocument();
+    expect(screen.getByText("https://www.remoterocketship.com/jobs/frontend-developer")).toBeInTheDocument();
+    expect(screen.getByText("Remote")).toBeInTheDocument();
+    expect(screen.getByText("04/08/2025")).toBeInTheDocument();
+    expect(screen.getByText("05/08/2025")).toBeInTheDocument();
+    expect(screen.getByText("25/08/2025")).toBeInTheDocument();
+
+    expect(screen.getByRole("button", { name: /close/i })).toBeInTheDocument();
+  });
 });
