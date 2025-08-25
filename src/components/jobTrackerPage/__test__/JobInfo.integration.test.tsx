18e83726c7dd6ff5b2aae68f75499244188de7d7
Dika Afty <dikaafty@gmail.com>
2025-08-25 22:02:20 +0700

    Create a test to check that isInfoOpen state from Redux store will be updated and JobInfo component will be close when close button is clicked

--- src/components/jobTrackerPage/__test__/JobInfo.integration.test.tsx
+++ src/components/jobTrackerPage/__test__/JobInfo.integration.test.tsx
@@ -2,5 +2,34 @@ import JobInfo from "../JobInfo";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("JobInfo component", () => {
-  
+  test("updates Redux store isInfoOpen state and closes the JobInfo when close button clicked", async () => {
+    const { store } = renderWithProvider(
+      <JobInfo />,
+      {
+        preloadedState: {
+          jobTracker: {
+            isInfoOpen: true,
+            selectedJob: {
+              jobTitle: "Front-End Developer",
+              jobUrl: "https://www.remoterocketship.com/jobs/frontend-developer",
+              jobLocation: "Remote",
+              companyName: "Link AI",
+              status: "Bookmarked",
+            },
+          }
+        },
+        storeType: "integration"
+      }
+    );
+
+    const user = userEvent.setup();
+
+    expect(screen.getByTestId(/job info/i)).toBeInTheDocument();
+    expect(store.getState().jobTracker.isInfoOpen).toBe(true);
+
+    await user.click(screen.getByRole("button", { name: /close/i }));
+
+    expect(screen.queryByTestId(/job info/i)).not.toBeInTheDocument();
+    expect(store.getState().jobTracker.isInfoOpen).toBe(false);
+  });
 });
