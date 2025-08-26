4ee8c062708c322e5d1ed1b5025b7c6b9b56a549
Dika Afty <dikaafty@gmail.com>
2025-08-26 20:09:59 +0700

    Create a test to check that isOpen state will be updated when add job button is clicked

--- src/components/jobTrackerPage/__test__/AddJobButton.integration.test.tsx
+++ src/components/jobTrackerPage/__test__/AddJobButton.integration.test.tsx
@@ -2,5 +2,27 @@ import AddJobButton from "../AddJobButton";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("AddJobButton component", () => {
-  
+  test("updates Redux store isOpen state when add job button clicked", async () => {
+    const { store } = renderWithProvider(
+      <AddJobButton />,
+      {
+        preloadedState: {
+          jobTracker: {
+            jobs: [],
+            activeCategory: "all",
+            isOpen: false,
+          }
+        },
+        storeType: "integration"
+      }
+    );
+
+    const user = userEvent.setup();
+
+    expect(store.getState().jobTracker.isOpen).toBe(false);
+
+    await user.click(screen.getAllByRole("button", { name: /add job/i })[0]);
+
+    expect(store.getState().jobTracker.isOpen).toBe(true);
+  });
 });
