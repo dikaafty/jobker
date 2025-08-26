07e1682909115619483c7f07ef49718ebc832a9f
Dika Afty <dikaafty@gmail.com>
2025-08-26 20:23:54 +0700

    Create a test to check that AboutHero component will be rendered with the correct elements

--- src/components/aboutPage/__test__/AboutHero.unit.test.tsx
+++ src/components/aboutPage/__test__/AboutHero.unit.test.tsx
@@ -2,5 +2,12 @@ import AboutHero from "../AboutHero";
 import { render, screen } from "@/lib/test-utils";
 
 describe("AboutHero component", () => {
-  
+  test("render AboutHero component with correct elements", () => {
+    render(<AboutHero />);
+
+    expect(screen.getByText(/about/i)).toBeInTheDocument();
+    expect(screen.getByText(/track smarter/i)).toBeInTheDocument();
+    expect(screen.getByText(/land faster/i)).toBeInTheDocument();
+    expect(screen.getByText(/jobker helps you organize/i)).toBeInTheDocument();
+  });
 });
