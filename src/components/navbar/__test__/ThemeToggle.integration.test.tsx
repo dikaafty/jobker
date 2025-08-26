dd532ca4fe8d96573425289c2f707231fb27afad
Dika Afty <dikaafty@gmail.com>
2025-08-27 00:19:18 +0700

    Declare beforeEach setup hook and its logic in ThemeToggle component's test suite

--- src/components/navbar/__test__/ThemeToggle.integration.test.tsx
+++ src/components/navbar/__test__/ThemeToggle.integration.test.tsx
@@ -3,4 +3,12 @@ import { screen, userEvent, render } from "@/lib/test-utils";
 
 describe("ThemeToggle component", () => {
   let user;
+
+  beforeEach(() => {
+    user = userEvent.setup();
+
+    localStorage.clear();
+    jest.spyOn(window.localStorage.__proto__, "setItem");
+    jest.spyOn(window.localStorage.__proto__, "getItem");
+  });
 });
