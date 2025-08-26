96fcfdd8f7a600f428f6b11618bee853ad35df56
Dika Afty <dikaafty@gmail.com>
2025-08-26 19:58:29 +0700

    Create a test to check that isOpen state will be updated when edit button is clicked

--- src/components/jobTrackerPage/__test__/JobTable.integration.test.tsx
+++ src/components/jobTrackerPage/__test__/JobTable.integration.test.tsx
@@ -2,5 +2,36 @@ import JobTable from "../JobTable";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("JobTable component", () => {
-  
+  test("updates Redux store isOpen state when edit button clicked", async () => {
+    const { store } = renderWithProvider(
+      <JobTable />,
+      {
+        preloadedState: {
+          jobTracker: {
+            jobs: [
+              {
+                id: 1,
+                jobTitle: "Front End Developer",
+                jobLocation: "Remote",
+                companyName: "Link AI",
+                status: "Applied",
+                dateSaved: "14/08/2025",
+              }
+            ],
+            activeCategory: "all",
+            isOpen: false,
+          }
+        },
+        storeType: "integration"
+      }
+    );
+
+    const user = userEvent.setup();
+
+    expect(store.getState().jobTracker.isOpen).toBe(false);
+
+    await user.click(screen.getAllByRole("button", { name: /edit/i })[0]);
+
+    expect(store.getState().jobTracker.isOpen).toBe(true);
+  });
 });
