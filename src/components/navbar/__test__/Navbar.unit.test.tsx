ea17b371336ec7180ffe0c4443e3e4bf577bbc46
Dika Afty <dikaafty@gmail.com>
2025-08-26 21:22:56 +0700

    Create a test to check that desktop navbar will be rendered with the correct elements

--- src/components/navbar/__test__/Navbar.unit.test.tsx
+++ src/components/navbar/__test__/Navbar.unit.test.tsx
@@ -2,5 +2,15 @@ import Navbar from "../Navbar";
 import { screen, renderWithRouter } from "@/lib/test-utils";
 
 describe("Navbar component", () => {
-  
+  test("renders desktop navbar with correct elements", () => {
+    renderWithRouter(<Navbar />);
+
+    expect(screen.getByRole("link", { name: /jobker/i })).toBeInTheDocument();
+
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Track Job" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "About" })).toBeInTheDocument();
+
+    expect(screen.getAllByRole("button")[1]).toBeInTheDocument(); // <= theme toggle button
+  });
 });
