4a7cd3e57d7b42e95aa74df62abb27c83b0eb941
Dika Afty <dikaafty@gmail.com>
2025-08-26 20:34:58 +0700

    Create a test to check that AboutContent component will be rendered with the correct elements

--- src/components/aboutPage/__test__/AboutContent.unit.test.tsx
+++ src/components/aboutPage/__test__/AboutContent.unit.test.tsx
@@ -2,5 +2,23 @@ import AboutContent from "../AboutContent";
 import { render, screen } from "@/lib/test-utils";
 
 describe("AboutContent component", () => {
-  
+  test("renders AboutContent component with correct elements", () => {
+    render(<AboutContent />);
+
+    expect(screen.getByText(/about/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/your career./i)).toBeInTheDocument();
+    expect(screen.getByText(/organized./i)).toBeInTheDocument();
+
+    expect(screen.getByText(/jobker helps you take control/i)).toBeInTheDocument();
+    expect(screen.getByText(/with jobker/i)).toBeInTheDocument();
+    expect(screen.getByText(/add job applications/i)).toBeInTheDocument();
+    expect(screen.getByText(/quickly log/i)).toBeInTheDocument();
+    expect(screen.getByText(/track your progress/i)).toBeInTheDocument();
+    expect(screen.getByText(/easily manage/i)).toBeInTheDocument();
+    expect(screen.getByText(/stay focused/i)).toBeInTheDocument();
+    expect(screen.getByText(/jobker helps you prioritize/i)).toBeInTheDocument();
+    expect(screen.getByText(/no more clutter/i)).toBeInTheDocument();
+    expect(screen.getByText(/just clarity/i)).toBeInTheDocument();
+  });
 });
