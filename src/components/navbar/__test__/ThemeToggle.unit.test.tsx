de9eaadf1affd2de64fa78bff6bb8a50972d67a8
Dika Afty <dikaafty@gmail.com>
2025-08-26 21:57:40 +0700

    Create a test to check that the theme toggle button's icon will be toggled if it gets clicked

--- src/components/navbar/__test__/ThemeToggle.unit.test.tsx
+++ src/components/navbar/__test__/ThemeToggle.unit.test.tsx
@@ -8,4 +8,23 @@ describe("ThemeToggle component", () => {
     expect(screen.getByRole("button"));
     expect(screen.getByTestId(/moon icon/i)).toBeInTheDocument();
   });
+
+  test("toggle icon when theme toggle button clicked", async () => {
+    render(<ThemeToggle />);
+
+    const user = userEvent.setup();
+
+    expect(screen.getByTestId(/moon icon/i)).toBeInTheDocument();
+    expect(screen.queryByTestId(/sun icon/i)).not.toBeInTheDocument();
+
+    await user.click(screen.getByRole("button"));
+
+    expect(screen.queryByTestId(/moon icon/i)).not.toBeInTheDocument();
+    expect(screen.getByTestId(/sun icon/i)).toBeInTheDocument();
+
+    await user.click(screen.getByRole("button"));
+
+    expect(screen.getByTestId(/moon icon/i)).toBeInTheDocument();
+    expect(screen.queryByTestId(/sun icon/i)).not.toBeInTheDocument();
+  });
 });
