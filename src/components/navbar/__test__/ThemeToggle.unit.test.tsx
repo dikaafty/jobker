41b50a1a250570b6bf3d7083f4549214ae812c09
Dika Afty <dikaafty@gmail.com>
2025-08-26 21:39:32 +0700

    Create a test to check that ThemeToggle component will be rendered with the correct elements

--- src/components/navbar/__test__/ThemeToggle.unit.test.tsx
+++ src/components/navbar/__test__/ThemeToggle.unit.test.tsx
@@ -2,5 +2,9 @@ import ThemeToggle from "../ThemeToggle";
 import { render, screen } from "@/lib/test-utils";
 
 describe("ThemeToggle component", () => {
-  
+  test("renders ThemeToggle component with correct elements", () => {
+    render(<ThemeToggle />);
+
+    expect(screen.getByRole("button"));
+  });
 });
