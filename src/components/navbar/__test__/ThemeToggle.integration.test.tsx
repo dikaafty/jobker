82d49f2b752b0087fff17e7c4084289315ac5b47
Dika Afty <dikaafty@gmail.com>
2025-08-27 00:27:56 +0700

    Create a test to check that when the theme is toggled it will store the theme value to localStorage

--- src/components/navbar/__test__/ThemeToggle.integration.test.tsx
+++ src/components/navbar/__test__/ThemeToggle.integration.test.tsx
@@ -11,4 +11,18 @@ describe("ThemeToggle component", () => {
     jest.spyOn(window.localStorage.__proto__, "setItem");
     jest.spyOn(window.localStorage.__proto__, "getItem");
   });
+
+  test("toggles theme and stores value in localStorage", async () => {
+    render(<ThemeToggle />);
+
+    expect(screen.getByTestId(/moon icon/i)).toBeInTheDocument();
+    expect(localStorage.setItem).toHaveBeenCalledWith("theme", "dark");
+
+    await user.click(screen.getByRole("button"));
+
+    expect(screen.getByTestId(/sun icon/i)).toBeInTheDocument();
+    expect(localStorage.setItem).toHaveBeenCalledWith("theme", "light");
+
+    expect(localStorage.getItem("theme")).toBe("light");
+  });
 });
