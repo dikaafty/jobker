f5e5b30e30034a157b3d995a872ce994b0f71f19
Dika Afty <dikaafty@gmail.com>
2025-08-27 16:59:26 +0700

    Create a test to check that job listings filter will be applied and then cleared when the same filter category button is clicked twice

--- src/pages/__test__/JobTrackerPage.integration.test.tsx
+++ src/pages/__test__/JobTrackerPage.integration.test.tsx
@@ -62,4 +62,59 @@ describe("JobTrackerPage component", () => {
     expect(screen.queryAllByText(/junior front end developer/i)[0]).toBeUndefined();
     expect(screen.getAllByText("Front End Developer")[0]).toBeInTheDocument();
   });
+
+  test("applies and then clears the filter when the same filter category button is clicked twice", async () => {
+    renderWithProvider(
+      <JobTrackerPage />,
+      {
+        preloadedState: {
+          jobTracker: {
+            activeCategory: "all",
+            filterCategories: [
+              { id: 1, category: "BOOKMARKED", numberOfItems: 1 },
+              { id: 2, category: "APPLYING", numberOfItems: 0 },
+              { id: 3, category: "APPLIED", numberOfItems: 1 },
+              { id: 4, category: "INTERVIEWING", numberOfItems: 0 },
+              { id: 5, category: "NEGOTIATING", numberOfItems: 0 },
+              { id: 6, category: "ACCEPTED", numberOfItems: 0 },
+            ],
+            jobs: [
+              {
+                id: 1,
+                jobTitle: "Junior Front End Developer",
+                jobUrl: "https://www.remoterocketship.com/jobs/front-end-developer/",
+                jobLocation: "Manchaster - Remote",
+                companyName: "Link AI",
+                status: "Applied",
+                dateSaved: "25/08/2025",
+              },
+              {
+                id: 2,
+                jobTitle: "Front End Developer",
+                jobUrl: "https://www.ziprrecuite.com/",
+                jobLocation: "Abu Dhabi - Remote",
+                companyName: "Kara AI",
+                status: "Bookmarked",
+                dateSaved: "27/08/2025",
+              }
+            ],
+          }
+        },
+        storeType: "integration"
+      }
+    );
+
+    expect(screen.getAllByText(/junior front end developer/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText("Front End Developer")[0]).toBeInTheDocument();
+
+    await user.click(screen.getByTestId(/applied button/i));
+
+    expect(screen.getAllByText(/junior front end developer/i)[0]).toBeInTheDocument();
+    expect(screen.queryAllByText("Front End Developer")[0]).toBeUndefined();
+
+    await user.click(screen.getByTestId(/applied button/i));
+
+    expect(screen.getAllByText(/junior front end developer/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText("Front End Developer")[0]).toBeInTheDocument();
+  });
 });
