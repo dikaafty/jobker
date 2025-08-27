ddc8bdd8a40fd37153e0705b3af6f3430d3d2b8e
Dika Afty <dikaafty@gmail.com>
2025-08-27 16:28:40 +0700

    Create user variable in JobTrackerPage component test suite

--- src/pages/__test__/JobTrackerPage.integration.test.tsx
+++ src/pages/__test__/JobTrackerPage.integration.test.tsx
@@ -2,5 +2,5 @@ import { JobTrackerPage } from "../JobTrackerPage";
 import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("JobTrackerPage component", () => {
-  
+  let user: ReturnType<typeof userEvent.setup>;
 });
