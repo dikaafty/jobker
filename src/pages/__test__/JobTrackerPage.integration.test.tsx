dd038d4886bff77a10e15126f9e0836452d495ce
Dika Afty <dikaafty@gmail.com>
2025-09-05 22:44:46 +0700

    Delete queryByTestId function import

--- src/pages/__test__/JobTrackerPage.integration.test.tsx
+++ src/pages/__test__/JobTrackerPage.integration.test.tsx
@@ -1,5 +1,5 @@
 import { JobTrackerPage } from "../JobTrackerPage";
-import { screen, userEvent, renderWithProvider, queryByTestId } from "@/lib/test-utils";
+import { screen, userEvent, renderWithProvider } from "@/lib/test-utils";
 
 describe("JobTrackerPage component", () => {
   let user: ReturnType<typeof userEvent.setup>;
