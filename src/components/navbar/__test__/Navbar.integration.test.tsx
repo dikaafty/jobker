025dbbb66f81229b9a5fee0ee8a708834372e01b
Dika Afty <dikaafty@gmail.com>
2025-09-06 21:41:18 +0700

    Import Homepage component

--- src/components/navbar/__test__/Navbar.integration.test.tsx
+++ src/components/navbar/__test__/Navbar.integration.test.tsx
@@ -1,3 +1,4 @@
 import { renderWithRouter, screen, userEvent } from "@/lib/test-utils";
 import { Route, Routes } from "react-router-dom";
+import { Homepage } from "@/pages/Homepage";
 import Navbar from "../Navbar";
