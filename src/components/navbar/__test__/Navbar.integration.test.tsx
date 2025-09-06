bd35ea94ca30502c42a7cd8fe5e2f515baf1404c
Dika Afty <dikaafty@gmail.com>
2025-09-06 21:41:42 +0700

    Import JobTrackerPage component

--- src/components/navbar/__test__/Navbar.integration.test.tsx
+++ src/components/navbar/__test__/Navbar.integration.test.tsx
@@ -1,4 +1,5 @@
 import { renderWithRouter, screen, userEvent } from "@/lib/test-utils";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "@/pages/Homepage";
+import { JobTrackerPage } from "@/pages/JobTrackerPage";
 import Navbar from "../Navbar";
