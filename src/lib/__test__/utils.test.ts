58bea117c96c5f3491f98f55f020d69bcebd1a91
Dika Afty <dikaafty@gmail.com>
2025-09-08 01:02:42 +0700

    Import runObserver and capitalize functions

--- src/lib/__test__/utils.test.ts
+++ src/lib/__test__/utils.test.ts
@@ -1,4 +1,4 @@
-import { cn } from "../utils";
+import { cn, runObserver, capitalize } from "../utils";
 
 test("combines class names from arrays/strings and applies Tailwind merge rules", () => {
   expect(cn(["text-sm font-jost font-bold", "text-foreground"]))
