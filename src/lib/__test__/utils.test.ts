167f6b010ec7d22765e007b4c48ca4222e1a2d75
Dika Afty <dikaafty@gmail.com>
2025-09-08 01:05:12 +0700

    Create a test for capitalize function

--- src/lib/__test__/utils.test.ts
+++ src/lib/__test__/utils.test.ts
@@ -4,3 +4,7 @@ test("combines class names from arrays/strings and applies Tailwind merge rules"
   expect(cn(["text-sm font-jost font-bold", "text-foreground"]))
   .toBe("text-sm font-jost font-bold text-foreground");
 });
+
+test("capitalize string", () => {
+  expect(capitalize("my name is antwon")).toBe("My name is antwon");
+});
