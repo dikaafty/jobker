0ac77a4eb91aed795a3a33033f718b59c03f7643
Dika Afty <dikaafty@gmail.com>
2025-09-08 01:20:20 +0700

    Create IntersectionObserverMock class

--- src/lib/__test__/utils.test.ts
+++ src/lib/__test__/utils.test.ts
@@ -1,5 +1,37 @@
 import { cn, runObserver, capitalize } from "../utils";
 
+class IntersectionObserverMock {
+  callback: IntersectionObserverCallback;
+  elements: Set<Element> = new Set();
+
+  constructor(callback: IntersectionObserverCallback) {
+    this.callback = callback;
+  };
+
+  observe = (element: Element) => {
+    this.elements.add(element);
+  };
+
+  unobserve = (element: Element) => {
+    this.elements.delete(element);
+  };
+
+  disconnect = () => {
+    this.elements.clear();
+  };
+
+  triggerIntersect = (isIntersecting: boolean, target: Element) => {
+    const entry = [
+      {
+        isIntersecting,
+        target
+      } as IntersectionObserverEntry,
+    ];
+    
+    this.callback(entry, this as unknown as IntersectionObserver);
+  };
+}
+
 test("combines class names from arrays/strings and applies Tailwind merge rules", () => {
   expect(cn(["text-sm font-jost font-bold", "text-foreground"]))
   .toBe("text-sm font-jost font-bold text-foreground");
