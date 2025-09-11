a2366609f5da5f6bf966151308bf575363ea1679
Dika Afty <dikaafty@gmail.com>
2025-09-11 22:19:05 +0700

    Create e2e test for deleting a job

--- cypress/e2e/jobTracker.cy.ts
+++ cypress/e2e/jobTracker.cy.ts
@@ -29,4 +29,40 @@ describe("Job Tracker", () => {
     cy.get("[data-testid='dialog']").should("not.exist");
     cy.contains(job.title).should("be.visible");
   });
+
+  it("should delete a job", () => {
+    const job = {
+      title: "Frontend Developer",
+      url: "https://lamarjob.com/",
+      location: "Jakarta, Indonesia",
+      company: "Bagus AI",
+      status: "Applied",
+      dateApplied: "2025-09-11",
+      deadline: "2025-10-01",
+    };
+
+    // Add a job first
+    cy.contains("Add Job").click();
+    cy.get("[data-testid='dialog']").should("be.visible");
+
+    cy.findByLabelText(/job title/i).type(job.title).should("have.value", job.title);
+    cy.findByLabelText(/job url/i).type(job.url).should("have.value", job.url);
+    cy.findByLabelText(/job location/i).type(job.location).should("have.value", job.location);
+    cy.findByLabelText(/company name/i).type(job.company).should("have.value", job.company);
+    cy.findByLabelText(/status/i).select(job.status).should("have.value", job.status);
+    cy.findByLabelText(/date applied/i).type(job.dateApplied).should("have.value", job.dateApplied);
+    cy.findByLabelText(/deadline/i).type(job.deadline).should("have.value", job.deadline);
+
+    cy.contains("Save Job").click();
+    cy.get("[data-testid='dialog']").should("not.exist");
+    cy.contains(job.title).should("be.visible");
+
+    // Delete the job
+    cy.get("[aria-label='edit']").first().click();
+    cy.get("[data-testid='dialog']").should("be.visible");
+    
+    cy.contains("Delete Job").click();
+    cy.contains(job.title).should("not.exist");
+    cy.get("[data-testid='dialog']").should("not.exist");
+  });
 });
